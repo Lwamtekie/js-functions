e8c7652fa3e12fe31f9ecc36856f843d8bb74639
Lwam Tekie <ltekie87@yahoo.com>
2019-03-02 11:07:43 -0600

    added names

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log('hi');
+// lwam Teikie
+// Britney spears
+// tori Amos
+// john doe
+
+const firstName = "lwam";
+const  lastName = "Tei";
+console.log(firstName + ' ' + lastName);
+console.log (`${firstName} ${lastName}`);
+const firstName1 = 'Britney';
+const lastName2 = 'spears';
+console. log(`${firstName1} ${lastName2}`);
+const namePrinter = (firstName, lastname) => {
+    console.log(`${firstName} ${lastName}`);
+};
+namePrinter('lwam', 'tekie');
+namePrinter('john', 'doe');
