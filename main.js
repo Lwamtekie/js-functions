5101a59f6ec39939671b3462809e1756423c1c8a
Lwam Tekie <ltekie87@yahoo.com>
2019-03-02 11:09:26 -0600

    namePrinter working

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ console.log('hi');
 // tori Amos
 // john doe
 
-const firstName = "lwam";
-const  lastName = "Tei";
-console.log(firstName + ' ' + lastName);
-console.log (`${firstName} ${lastName}`);
-const firstName1 = 'Britney';
-const lastName2 = 'spears';
-console. log(`${firstName1} ${lastName2}`);
-const namePrinter = (firstName, lastname) => {
+// const firstName = "lwam";
+// const  lastName = "Tei";
+// console.log(firstName + ' ' + lastName);
+// console.log (`${firstName} ${lastName}`);
+// const firstName1 = 'Britney';
+// const lastName2 = 'spears';
+// console. log(`${firstName1} ${lastName2}`);
+
+const namePrinter = (firstName, lastName) => {
     console.log(`${firstName} ${lastName}`);
 };
 namePrinter('lwam', 'tekie');
